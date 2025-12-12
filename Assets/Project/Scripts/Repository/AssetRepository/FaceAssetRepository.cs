339f54f3d317058c199f57e11a9c6ae50ac31b90
purple-comet <131944632+purple-comet@users.noreply.github.com>
2025-12-12 13:54:49 +0900

    Feat: FaceAssetRepositoryを実装

---
+++ Assets/Project/Scripts/Repository/AssetRepository/FaceAssetRepository.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Cysharp.Text;
+using Project.Scripts.Extensions;
+
+namespace Project.Scripts.Repository.AssetRepository
+{
+    public class FaceAssetRepository : AssetRepositoryBase
+    {
+        public Sprite Load(string charaName, string expression)
+        {
+            string address = ZString.Format(
+                "{0}/Character/{1}/Face/{1}{2}_Face.png",
+                GamePath.TexturesPath,
+                charaName,
+                $"_{expression}"
+            );
+
+            Sprite asset = Addressables.LoadAssetAsync<Sprite>(address).WaitForCompletion();
+            return asset;
+        }
+    }
+}
