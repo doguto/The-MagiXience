c79f94b4c5ddbbb24d151e2153db91a147cb5f29
kuto <doguto110@gmail.com>
2026-01-02 22:47:29 +0900

    Feat: インゲーム(シューティングorシナリオ)かどうかの判定をRuntimeModelに追加

--- Assets/Project/Scripts/Model/RuntimeModel.cs
+++ Assets/Project/Scripts/Model/RuntimeModel.cs
@@ -3,5 +3,6 @@
     public class RuntimeModel : ModelBase
     {
         public int CurrentStageNumber { get; internal set; } = -1;
+        public bool IsInGame => CurrentStageNumber != -1;
     }
 }
