c9aca4ee0ce7d16ce6b2e1fe01dc69336a1dbd12
kuto <doguto110@gmail.com>
2026-04-23 21:10:02 +0900

    Feat: BulletのPrefabを作成出来るようにEditorを設定

--- Assets/Project/Scenes/Battle/Editor/BattlePrefabCreator.cs
+++ Assets/Project/Scenes/Battle/Editor/BattlePrefabCreator.cs
@@ -7,6 +7,7 @@ namespace Project.Scenes.Battle.Editor
     public static class BattlePrefabCreator
     {
         const string SourcePrefabPath = "Assets/Project/Scenes/Battle/Prefabs/Base/EnemyEntityBase.prefab";
+        const string BulletPrefabPath = "Assets/Project/Scenes/Battle/Prefabs/Base/Bullet.prefab";
 
         [MenuItem("Assets/Create/Battle/Enemy Entity Base")]
         public static void CreateEnemyEntityBase()
@@ -24,6 +25,26 @@ namespace Project.Scenes.Battle.Editor
 
         [MenuItem("Assets/Create/Battle/Enemy Entity Base", true)]
         public static bool CreateEnemyEntityBaseValidation()
+        {
+            return !string.IsNullOrEmpty(GetSelectedFolderPath());
+        }
+
+        [MenuItem("Assets/Create/Battle/Bullet")]
+        public static void CreateBullet()
+        {
+            var targetFolder = GetSelectedFolderPath();
+            var newPath = AssetDatabase.GenerateUniqueAssetPath(targetFolder + "/Bullet.prefab");
+
+            AssetDatabase.CopyAsset(BulletPrefabPath, newPath);
+            AssetDatabase.Refresh();
+
+            var newAsset = AssetDatabase.LoadAssetAtPath<Object>(newPath);
+            Selection.activeObject = newAsset;
+            EditorGUIUtility.PingObject(newAsset);
+        }
+
+        [MenuItem("Assets/Create/Battle/Bullet", true)]
+        public static bool CreateBulletValidation()
         {
             return !string.IsNullOrEmpty(GetSelectedFolderPath());
         }
