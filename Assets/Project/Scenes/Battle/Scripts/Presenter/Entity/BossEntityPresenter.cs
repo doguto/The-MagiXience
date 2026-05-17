2c69ea50ab5715b927ba7f84940ee55002364102
purple-comet <131944632+purple-comet@users.noreply.github.com>
2026-05-17 12:37:51 +0900

    Feat: ボスにダメージを与えた時の演出を実装

--- Assets/Project/Scenes/Battle/Scripts/Presenter/Entity/BossEntityPresenter.cs
+++ Assets/Project/Scenes/Battle/Scripts/Presenter/Entity/BossEntityPresenter.cs
@@ -27,6 +27,10 @@ namespace Project.Scenes.Battle.Scripts.Presenter.Entity
         [SerializeField] int bulletDamage = 10;
         [SerializeField] GameObject[] enemySpawnPrefabs;
 
+        [Header("Damage Flash")]
+        [SerializeField] float damageFlashInterval = 0.05f;
+        [SerializeField] float damageFlashDuration = 0.2f;
+
         [Header("Component References")]
         [SerializeField] EnemyEntityView view;
         EnemyTracker enemyTracker;
@@ -75,6 +79,7 @@ namespace Project.Scenes.Battle.Scripts.Presenter.Entity
         Tween entranceTween;
         CancellationTokenSource movementCts;
         readonly CompositeDisposable disposables = new();
+        IDisposable damageFlashSubscription;
 
         public EnemyEntityModel Model => model;
         public IObservable<Unit> OnDeath => model?.OnDeath;
@@ -91,7 +96,50 @@ namespace Project.Scenes.Battle.Scripts.Presenter.Entity
                 .Subscribe(_ => HandleDeath())
                 .AddTo(disposables);
 
+            SubscribeToDamageFlash();
+
             view.UpdatePosition(transform.position);
+        }
+
+        void SubscribeToDamageFlash()
+        {
+            int previousHp = model.CurrentHp.Value;
+            model.CurrentHp
+                .Skip(1)
+                .Subscribe(hp =>
+                {
+                    if (hp < previousHp)
+                    {
+                        PlayDamageFlash();
+                    }
+                    previousHp = hp;
+                })
+                .AddTo(disposables);
+        }
+
+        void PlayDamageFlash()
+        {
+            damageFlashSubscription?.Dispose();
+
+            view.SetDamageFlashActive(true);
+            bool flashOn = true;
+            float elapsed = 0f;
+
+            damageFlashSubscription = Observable
+                .Interval(TimeSpan.FromSeconds(damageFlashInterval))
+                .Subscribe(_ =>
+                {
+                    elapsed += damageFlashInterval;
+                    if (elapsed >= damageFlashDuration)
+                    {
+                        view.ResetDamageFlash();
+                        damageFlashSubscription?.Dispose();
+                        damageFlashSubscription = null;
+                        return;
+                    }
+                    flashOn = !flashOn;
+                    view.SetDamageFlashActive(flashOn);
+                });
         }
 
         public void OnPhaseStarted(BattlePhaseModelBase phase, BattleTimelineBuilderAsset builder)
@@ -317,6 +365,7 @@ namespace Project.Scenes.Battle.Scripts.Presenter.Entity
         void OnDestroy()
         {
             StopMovement();
+            damageFlashSubscription?.Dispose();
             disposables.Dispose();
             model?.AttackStrategy?.Dispose();
             model?.Dispose();
--- Assets/Project/Scenes/Battle/Scripts/View/Entity/EnemyEntityView.cs
+++ Assets/Project/Scenes/Battle/Scripts/View/Entity/EnemyEntityView.cs
@@ -1,6 +1,50 @@
+using UnityEngine;
+
 namespace Project.Scenes.Battle.Scripts.View.Entity
 {
     public class EnemyEntityView : EntityViewBase
     {
+        [Header("Damage Flash")]
+        [SerializeField] SpriteRenderer bodySpriteRenderer;
+        [SerializeField] Color flashColor = Color.black;
+
+        Color originalBodyColor;
+        bool originalColorCaptured;
+
+        void Reset()
+        {
+            bodySpriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        protected override void OnAwakeView()
+        {
+            if (bodySpriteRenderer == null)
+            {
+                bodySpriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (bodySpriteRenderer != null)
+            {
+                originalBodyColor = bodySpriteRenderer.color;
+                originalColorCaptured = true;
+            }
+        }
+
+        public void SetDamageFlashActive(bool active)
+        {
+            if (bodySpriteRenderer == null) return;
+            if (!originalColorCaptured)
+            {
+                originalBodyColor = bodySpriteRenderer.color;
+                originalColorCaptured = true;
+            }
+            bodySpriteRenderer.color = active ? flashColor : originalBodyColor;
+        }
+
+        public void ResetDamageFlash()
+        {
+            if (bodySpriteRenderer == null) return;
+            if (!originalColorCaptured) return;
+            bodySpriteRenderer.color = originalBodyColor;
+        }
     }
 }
