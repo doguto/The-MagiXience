cfcd890dff421e7e3982bf182619b386c7c91812
purple-comet <131944632+purple-comet@users.noreply.github.com>
2026-05-23 17:16:31 +0900

    Feat: Addressables関連処理をAssetRepositoryに移動

--- Assets/Project/Scenes/Entry/Scripts/Presenter/EntryScenePresenter.cs
+++ Assets/Project/Scenes/Entry/Scripts/Presenter/EntryScenePresenter.cs
@@ -1,18 +1,18 @@
 using Cysharp.Threading.Tasks;
 using Project.Scripts.Extensions;
+using Project.Scripts.Model;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
 
 public class EntryScenePresenter : MonoBehaviour
 {
     async void Awake()
     {
-
         var globalSceneName = SceneType.Global.ToSceneName();
         var titleSceneName = SceneType.Title.ToSceneName();
 
-        await Addressables.InitializeAsync().ToUniTask();
+        var addressablesModel = new AddressablesModel();
+        await addressablesModel.InitializeAsync();
 
         await SceneManager.LoadSceneAsync(globalSceneName, LoadSceneMode.Additive).ToUniTask();
         await SceneManager.LoadSceneAsync(titleSceneName, LoadSceneMode.Additive).ToUniTask();
