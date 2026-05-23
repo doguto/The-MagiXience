e0a67d723911f45fe9ee7d4975922b82c33c1443
purple-comet <131944632+purple-comet@users.noreply.github.com>
2026-05-23 17:17:29 +0900

    Fix: エディタでEntryシーンから起動した時にデッドロックでフリーズしてしまう問題の解消

--- Assets/Project/Editor/GlobalSceneAutoLoader.cs
+++ Assets/Project/Editor/GlobalSceneAutoLoader.cs
@@ -9,27 +9,28 @@ namespace Project.Editor
     public class GlobalSceneAutoLoader
     {
         const string GlobalScenePath = "Assets/Project/Scenes/Global.unity";
-
+        
         static GlobalSceneAutoLoader()
         {
             EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
         }
-
+    
         static void OnPlayModeStateChanged(PlayModeStateChange state)
         {
             if (state != PlayModeStateChange.ExitingEditMode) return;
             EnsureGlobalSceneLoaded();
         }
-
+    
         static void EnsureGlobalSceneLoaded()
         {
-            // 既にGlobalSceneがロードされているかチェック
+            // 既にGlobalSceneがロードされている場合・EntrySceneから起動した場合はスキップ
             for (var i = 0; i < SceneManager.sceneCount; i++)
             {
                 var scene = SceneManager.GetSceneAt(i);
                 if (scene.name == SceneType.Global.ToSceneName()) return;
+                if (scene.name == SceneType.Entry.ToSceneName()) return;
             }
-
+    
             // GlobalSceneをAdditiveモードでロード
             EditorSceneManager.OpenScene(GlobalScenePath, OpenSceneMode.Additive);
         }
